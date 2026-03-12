fd99de6c2bd1622aed18cf0ccd5f0471e48d2c23
HTht-iaC <7T70T70TT@gmail.com>
2026-03-12 13:04:36 +0200

    tryin

--- terraform/ec2-gitlab-runner/install-runner.sh
+++ terraform/ec2-gitlab-runner/install-runner.sh
@@ -15,3 +15,10 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 yum install -y terraform vault
 yum install -y jq
+wget https://github.com/aquasecurity/trivy/releases/download/v0.45.0/trivy_0.45.0_Linux-64bit.rpm
+rpm -ivh trivy_0.45.0_Linux-64bit.rpm
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
+unzip sonar-scanner-cli-5.0.1.3006-linux.zip
+mv sonar-scanner-5.0.1.3006-linux /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' > /etc/profile.d/sonar.sh
+chmod +x /etc/profile.d/sonar.sh
